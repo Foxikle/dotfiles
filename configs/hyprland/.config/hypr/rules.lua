73925d87d1fa537a614f168593bd29121490ac33
Foxikle <foxikle@cytonic.net>
2026-09-03 07:48:55 -0400

    Make workspace 1 the default on framework

--- configs/hyprland/.config/hypr/rules.lua
+++ configs/hyprland/.config/hypr/rules.lua
@@ -3,31 +3,45 @@ local monitor = require("monitors")
 
 -- workspaces
 if helpers.get_hostname() == "framework" then
-  for i = 1, 10 do
-    hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.primary.output, persistent = true })
-  end
+	for i = 1, 10 do
+		if i == 1 then
+			hl.workspace_rule({
+				workspace = i,
+				monitor = monitor.MONITORS.primary.output,
+				persistent = true,
+				default = true,
+			})
+		else
+			hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.primary.output, persistent = true })
+		end
+	end
 else
-  for i = 1, 5 do
-    hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.primary.output, persistent = true })
-  end
+	for i = 1, 5 do
+		hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.primary.output, persistent = true })
+	end
 
-  for i = 6, 10 do
-    hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.secondary.output, persistent = true })
-  end
+	for i = 6, 10 do
+		hl.workspace_rule({ workspace = i, monitor = monitor.MONITORS.secondary.output, persistent = true })
+	end
 end
 
 -- window rules
 local window_rules = {
-  { match = { title = "^(btop|update-sys)$" },                           float = true },
-  { match = { title = "^(win.*)$", class = "^(.*jetbrains.*)$" },        no_initial_focus = true },
-  { match = { title = "^$", class = "^(jetbrains-\\w+)", float = true }, stay_focused = true },
-  { match = { title = "^(nvim.*)$", class = "^(kitty)$" },               opacity = "0.8" },
-  { match = { class = "^(kitty)$" },                                     opacity = "0.8" },
-  { match = { class = "^(kitty)$", title = "^(update-sys)$" } },
-  { match = { class = "^(thunar)$" },                                    size = { 800, 400 },    float = true, opacity = "0.8" },
-  { match = { class = "^(jetbrains-.*)$" },                              opacity = "0.9" },
+	{ match = { title = "^(btop|update-sys)$" }, float = true },
+	{ match = { title = "^(win.*)$", class = "^(.*jetbrains.*)$" }, no_initial_focus = true },
+	{ match = { title = "^$", class = "^(jetbrains-\\w+)", float = true }, stay_focused = true },
+	{ match = { title = "^(nvim.*)$", class = "^(kitty)$" }, opacity = "0.8" },
+	{ match = { class = "^(kitty)$" }, opacity = "0.8" },
+	{ match = { class = "^(kitty)$", title = "^(update-sys)$" } },
+	{
+		match = { class = "^(thunar)$" },
+		size = { 800, 400 },
+		float = true,
+		opacity = "0.8",
+	},
+	{ match = { class = "^(jetbrains-.*)$" }, opacity = "0.9" },
 }
 
 for _, rule in ipairs(window_rules) do
-  hl.window_rule(rule)
+	hl.window_rule(rule)
 end
